7f15a7208d0d49b55df96e58155bee8a1bb260c0
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-09-22 16:48:19 -0700

    Added states to dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,70 @@
 // import { ICountry, IState, ICity } from 'country-state-city'
 
 // Variables
-var inputAddress = document.querySelector('#inputAddress');
-var inputCity = document.querySelector('#inputCity');
-var inputState = document.querySelector('#inputCity');
-var mapBtn = document.querySelector('#mapBtn');
-var tableContainer = document.querySelector('.table-container');
+var inputAddress = document.querySelector("#inputAddress");
+var inputCity = document.querySelector("#inputCity");
+var inputState = document.querySelector("#inputState");
+var mapBtn = document.querySelector("#mapBtn");
+var tableContainer = document.querySelector(".table-container");
+
+var states = [
+  "AL",
+  "AK",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "OH",
+  "OK",
+  "OR",
+  "PA",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
+];
+
+getStates();
+function getStates() {
+  for (i = 0; i < states.length; i++) {
+    var state = document.createElement("option");
+    state.innerHTML = states[i];
+    inputState.append(state);
+  }
+}
