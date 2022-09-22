49f1b93f152e1dccc81c6435cce4432d8699cf3c
Crystal Aguilar <crystal_0688@yahoo.com>
2022-09-22 16:42:16 -0700

    emoty commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 //data source for CountQueuingStrategy, city and state: (https://github.com/dr5hn/countries-states-cities-database);
-npm i country-state-city
+// npm i country-state-city
 
-import csc from 'country-state-city';
+// import csc from 'country-state-city';
 
-import { Country, State, City }  from 'country-state-city';
-console.log(Country.getAllCountries())
-console.log(State.getAllStates())
+// import { Country, State, City }  from 'country-state-city';
+// console.log(Country.getAllCountries())
+// console.log(State.getAllStates())
 
-// Import Interfaces`
-import { ICountry, IState, ICity } from 'country-state-city'
+// // Import Interfaces`
+// import { ICountry, IState, ICity } from 'country-state-city'
+
+// Variables
+var inputAddress = document.querySelector('#inputAddress');
+var inputCity = document.querySelector('#inputCity');
+var inputState = document.querySelector('#inputCity');
+var mapBtn = document.querySelector('#mapBtn');
+var tableContainer = document.querySelector('.table-container');
