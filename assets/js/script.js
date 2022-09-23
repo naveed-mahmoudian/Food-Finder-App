df11b88fb8507fb82f5dd6a48ec562a890a3b631
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-09-22 19:06:19 -0700

    working on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-//data source for CountQueuingStrategy, city and state: (https://github.com/dr5hn/countries-states-cities-database);
-// npm i country-state-city
-
-// import csc from 'country-state-city';
-
-// import { Country, State, City }  from 'country-state-city';
-// console.log(Country.getAllCountries())
-// console.log(State.getAllStates())
-
-// // Import Interfaces`
-// import { ICountry, IState, ICity } from 'country-state-city'
-
 // Variables
 var inputAddress = $("#inputAddress");
 var inputCity = $("#inputCity");
@@ -98,35 +86,38 @@ function getAddress(event) {
 }
 
 //recent searches
-function history() {
-    var lastAreaSearched = getAddress(fullAddress)
-    localStorage.getItem(lastAreaSearched)
-    lastAreaSearched.push(history)
-    localStorage.setItem("fullAddress", lastAreaSearched)
-}
+// function history() {
+//   var lastAreaSearched = getAddress(fullAddress);
+//   localStorage.getItem(lastAreaSearched);
+//   lastAreaSearched.push(history);
+//   localStorage.setItem("fullAddress", lastAreaSearched);
+// }
 
-history();
-console.log(history)
+// history();
+// console.log(history);
 
 //crime data api code
-const crimes = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '46d9ba4524msh1bfc5bf71bc0638p1f849fjsnb2b8ad21547f',
-		'X-RapidAPI-Host': 'jgentes-Crime-Data-v1.p.rapidapi.com'
-	}
-};
+// const crimes = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "46d9ba4524msh1bfc5bf71bc0638p1f849fjsnb2b8ad21547f",
+//     "X-RapidAPI-Host": "jgentes-Crime-Data-v1.p.rapidapi.com",
+//   },
+// };
 
-fetch('https://jgentes-crime-data-v1.p.rapidapi.com/crime?startdate=9%2F19%2F2015&enddate=9%2F25%2F2015&long=-122.5076392&lat=37.757815', crimes)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch(
+//   "https://jgentes-crime-data-v1.p.rapidapi.com/crime?startdate=9%2F19%2F2015&enddate=9%2F25%2F2015&long=-122.5076392&lat=37.757815",
+//   crimes
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
 
-//for (crime in crimes) {
-    var crime = JSON.parse('crimes')
-    var tableContainer = documen.getElementById('table-container')
-    localStorage.getItem('tableContainer', crimes)
-    tableContainer.push('.table-container')
-    localStorage.setItem('tableContainer', tableContainer)
-    console.log(typeof, crime)
-//}
+// //for (crime in crimes) {
+// var crime = JSON.parse("crimes");
+// var tableContainer = document.getElementById("table-container");
+// localStorage.getItem("tableContainer", crimes);
+// tableContainer.push(".table-container");
+// localStorage.setItem("tableContainer", tableContainer);
+// console.log(typeof crime);
+// //}
